f0e8bd0db01b8759b7494c9d59aebcbc4660cd1f
ddamjengi <silluat11@gmail.com>
2020-01-13 17:42:58 +0900

    routes writing...

---
+++ init.js
@@ -0,0 +1,12 @@
+import app from "./app";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const handleListening = () => {
+  console.log(`🟢  Listening on : http://localhost:${PORT}`);
+};
+
+app.listen(PORT, handleListening);
---
+++ routes.js
@@ -0,0 +1,18 @@
+//  Global
+const HOME = "/";
+const JOIN = "/join";
+const LOGIN = "/login";
+const LOGOUT = "/logout";
+const SEARCH = "/search";
+
+const INFO = "/info";
+const WORSHIP = "/worship";
+const LIFE = "/life";
+const SCHOOL = "/school";
+const COMMUNITY = "/community";
+const STORY = "/story";
+
+const USER = "/users";
+const USER_DETAIL = "/:id";
+const USER_PROFILE = "/edit-profile";
+const CHANGE_PASSWORD = "/change-password";
