7f8b9b895fa231844fc5272bf68b47fa0b4972a2
ddamjengi <silluat11@gmail.com>
2020-01-21 23:12:33 +0900

    searching bar box-shadow

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,17 +1,20 @@
 const searchingIcon = document.getElementById("jsSearchingIcon");
 const mainLogo = document.getElementById("jsMainlogo");
-const userImg = document.getElementById("jsUserImg");
+const userIcon = document.getElementById("jsUserIcon");
 const searchingBar = document.getElementById("jsSearchingBar");
-const Nipple = document.getElementById("jsSearchingNipple");
+const nipple = document.getElementById("jsSearchingNipple");
+const nippleSmall = document.getElementById("jsNippleSmall");
 const header = document.getElementById("jsHeader");
-
+console.log(nippleSmall.className);
 function handleHidden() {
   if (searchingBar.className === "searching-bar hidden") {
     searchingBar.classList.remove("hidden");
-    Nipple.classList.remove("hidden");
+    nipple.classList.remove("hidden");
+    nippleSmall.classList.remove("hidden");
   } else {
     searchingBar.classList.add("hidden");
-    Nipple.classList.add("hidden");
+    nipple.classList.add("hidden");
+    nippleSmall.classList.add("hidden");
   }
 }
 
@@ -20,10 +23,14 @@ function handleScroll() {
     header.classList.add("header-scroll");
     header.classList.remove("header");
     mainLogo.src = "icon-img/logo_bottom.png";
+    searchingIcon.src = "icon-img/search_icon-black.png";
+    userIcon.src = "icon-img/preist_icon-black.png";
   } else {
     header.classList.remove("header-scroll");
     header.classList.add("header");
     mainLogo.src = "icon-img/logo_top.png";
+    searchingIcon.src = "icon-img/search_icon.png";
+    userIcon.src = "icon-img/preist_icon.png";
   }
 }
 
