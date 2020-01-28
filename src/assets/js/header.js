1fdd1cc5bb46b0fa63fff6785ce89f92bd6a987b
ddamjengi <silluat11@gmail.com>
2020-01-29 01:24:34 +0900

    header scss modify

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -18,13 +18,29 @@ function handleHidden() {
 }
 
 function handleScroll() {
-  if (window.scrollY > 0) {
-    header.classList.add("header-scroll");
-    header.classList.remove("header");
-    mainLogo.src = "icon-img/logo_bottom.png";
+  if (window.scrollY > 0 && window.scrollY < 10) {
+    if (header.className === "header-min-scroll") {
+      header.classList.add("header-scroll");
+      header.classList.remove("header-min-scroll");
+      header.style.animation = "scroll-slider-up 0.2s linear forwards";
+      mainLogo.src = "icon-img/벧엘교회_로고_over.png";
+      searchingIcon.src = "icon-img/search_icon-black.png";
+      userIcon.src = "icon-img/preist_icon-black.png";
+    } else {
+      header.classList.add("header-scroll");
+      header.classList.remove("header");
+      mainLogo.src = "icon-img/벧엘교회_로고_over.png";
+      searchingIcon.src = "icon-img/search_icon-black.png";
+      userIcon.src = "icon-img/preist_icon-black.png";
+    }
+  } else if (window.scrollY >= 10) {
+    header.classList.add("header-min-scroll");
+    header.style.animation = "scroll-slider-down 0.2s linear forwards";
+    header.classList.remove("header-scroll");
+    mainLogo.src = "icon-img/벧엘교회_로고_over.png";
     searchingIcon.src = "icon-img/search_icon-black.png";
     userIcon.src = "icon-img/preist_icon-black.png";
-  } else {
+  } else if (window.scrollY === 0) {
     header.classList.remove("header-scroll");
     header.classList.add("header");
     mainLogo.src = "icon-img/logo_top.png";
