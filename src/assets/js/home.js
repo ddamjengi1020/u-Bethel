7040af8ec42bf332decbae09b193968cf080f27d
ddamjengi <silluat11@gmail.com>
2020-01-31 20:15:46 +0900

    bethel-story slider event f

--- src/assets/js/home.js
+++ src/assets/js/home.js
@@ -1,6 +1,11 @@
 const mainImg = document.getElementById("jsMainImg");
 const arrowLeft = document.getElementById("jsArrowLeft");
 const arrowRight = document.getElementById("jsArrowRight");
+const redLeft = document.getElementById("jsRedLeft");
+const redLeftArrow = redLeft.querySelector("img");
+const redRight = document.getElementById("jsRedRight");
+const redRightArrow = redRight.querySelector("img");
+const sliderMaskBox = document.getElementById("jsSliderMaskBox");
 
 const imageList = [
   "icon-img/Slide_img1.png",
@@ -29,9 +34,35 @@ function handlePreImg() {
   mainImg.className = `main-img slider${num}`;
 }
 
+function handleLeftSlider() {
+  sliderMaskBox.style.animation = "bethelstoryDefault 1s linear forwards";
+}
+
+function handleRightSlider() {
+  sliderMaskBox.style.animation = "bethelstorySlider 1s linear forwards";
+}
+
+function handleRedLeftArrow() {
+  redLeftArrow.src = "icon-img/arrow_red_left.png";
+  redLeft.addEventListener("mouseleave", () => {
+    redLeftArrow.src = "icon-img/arrow_normal_left.png";
+  });
+}
+
+function handleRedRightArrow() {
+  redRightArrow.src = "icon-img/arrow_red_right.png";
+  redRight.addEventListener("mouseleave", () => {
+    redRightArrow.src = "icon-img/arrow_normal_right.png";
+  });
+}
+
 function init() {
   arrowRight.addEventListener("click", handleNextImg);
   arrowLeft.addEventListener("click", handlePreImg);
+  redLeft.addEventListener("click", handleLeftSlider);
+  redRight.addEventListener("click", handleRightSlider);
+  redLeft.addEventListener("mouseover", handleRedLeftArrow);
+  redRight.addEventListener("mouseover", handleRedRightArrow);
 }
 
 init();
