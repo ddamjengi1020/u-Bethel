e5bde7ab7e58c31f031ff856f468c32b47473e89
ddamjengi <silluat11@gmail.com>
2020-02-06 22:59:53 +0900

    paging test

--- src/assets/js/searchApi.js
+++ src/assets/js/searchApi.js
@@ -4,6 +4,8 @@ const testApi = document.getElementById("jsApiTest");
 const testWorshipsUl = document.getElementById("jsWorshipDb");
 const testLivesUl = document.getElementById("jsLivesDb");
 const testStoriesUl = document.getElementById("jsStoriesDb");
+const preBtn = document.getElementById("jsPre");
+const nextBtn = document.getElementById("jsNext");
 
 async function handleTest(e) {
   await axios
@@ -12,30 +14,67 @@ async function handleTest(e) {
     })
     .then(response => (testApi.innerHTML = response.data));
 }
-
+// ul.dataset.totalnum
 function searchDataApi(ul) {
-  let totalData = ul.dataset.totalnum; //총 데이터 수
+  let totalData = 1000; //총 데이터 수
   let dataPage = 10; // 한 페이지에 나타낼 데이터 수
   // let pageCount = 10; //한 화면에 나타낼 페이지 수
   let totalPage = Math.ceil(totalData / dataPage); //총 페이지 수
   // let pageGroup = Math.ceil(currentPage / pageCount);
-  for (let i = 1; i <= totalPage; i++) {
+  let num = 1;
+  for (let i = 0; i < totalPage; i++) {
     const button = document.createElement("button");
-    button.innerText = i;
-    ul.appendChild(button);
+    button.innerText = i + 1;
+    if (button.innerText <= dataPage) {
+      ul.appendChild(button);
+    }
   }
+
+  function handleNext() {
+    ++num;
+    ul.innerHTML = "";
+    for (let i = 0; i < totalPage; i++) {
+      const button = document.createElement("button");
+      button.innerText = i + 1;
+      if (
+        button.innerText > dataPage * (num - 1) &&
+        button.innerText <= dataPage * num
+      ) {
+        ul.appendChild(button);
+      }
+    }
+  }
+
+  function handlePre() {
+    --num;
+    ul.innerHTML = "";
+    for (let i = 0; i < totalPage; i++) {
+      const button = document.createElement("button");
+      button.innerText = i + 1;
+      if (
+        button.innerText > dataPage * (num - 1) &&
+        button.innerText <= dataPage * num
+      ) {
+        ul.appendChild(button);
+      }
+    }
+  }
+
+  nextBtn.addEventListener("click", handleNext);
+  preBtn.addEventListener("click", handlePre);
 
   const btnArr = Array.from(ul.querySelectorAll("button"));
 
   btnArr.forEach(el => {
+    console.log(btnArr);
     el.addEventListener("click", handleTest);
   });
 }
 
 function init() {
   searchDataApi(testWorshipsUl);
-  searchDataApi(testLivesUl);
-  searchDataApi(testStoriesUl);
+  // searchDataApi(testLivesUl);
+  // searchDataApi(testStoriesUl);
 }
 
 init();
